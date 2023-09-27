71eb61b7c327b99101b9948bfa3517f45aa253b4
Ananya1315 <138516311+Ananya1315@users.noreply.github.com>
2023-09-27 23:22:24 +0530

    Update fapi.py

--- fapi.py
+++ fapi.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
 app = Flask(__name__)
-call_count = 0  # Initialize the call count variable
+count = 0  # Initialize the call count variable
 
 @app.route('/index', methods=['GET'])
 def hello_world():
-    global call_count  
-    call_count += 1  
+    global count  
+    count += 1  
     return f'Hello World The end point has been called for {call_count} times.'
 
 if __name__ == '__main__':
